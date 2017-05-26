567b43e015578f27ef7d0618463bc02a39ae2ac2
OlegVzukov <baNANAmanchic@gmail.com>
2017-05-26 19:40:18 +0300

    update v0.2

--- mlpost/mlpost/urls.py
+++ mlpost/mlpost/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mostlikes import views
+
 
 urlpatterns = [
+    url(r'^api/mlpost', views.MLPostView),
+    url(r'^', views.IndexView),
     url(r'^admin/', admin.site.urls),
 ]
--- mlpost/mostlikes/views.py
+++ mlpost/mostlikes/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+import requests
+import json
+import datetime
+
 
 # Create your views here.
+
+def IndexView(request):
+    print(request.GET['id'])
+    print(request.GET['time'])
+    return render(request, "index.html", locals())
+
+
+def MLPostView(request):
+
+    wall_name = request.GET["id"]
+    posts_date = int(request.GET["time"])
+
+    posts = requests.get(
+        "https://api.vk.com/method/wall.get?domain={0}&count=10000&extended=1".format(wall_name)).text
+    posts = json.loads(posts)["response"]["wall"]
+    posts = posts[1: len(posts)]
+
+    most_likes = 0
+
+    for post in posts:
+
+        if ((datetime.datetime.now()) - datetime.datetime.fromtimestamp(post["date"])).days < posts_date:
+
+            if post["likes"]["count"] > most_likes:
+                most_likes = post["likes"]["count"]
+                wall_id = post["from_id"]
+                post_id = post["id"]
+                post_date = datetime.datetime.fromtimestamp(post["date"])
+
+                output_link = "https://vk.com/wall{0}_{1}".format(wall_id, post_id)
+
+    output = json.dumps({"link": output_link, "likes_count": most_likes})
+
+    return render(request, "index.html", locals())
